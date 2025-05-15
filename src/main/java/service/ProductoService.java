09b92bc9944a2778ef634abd7f90255ad2cf3a45
ander <andersonsoto102@gmail.com>
2025-05-14 23:13:38 -0500

    Recuperacion

--- src/main/java/service/ProductoService.java
+++ src/main/java/service/ProductoService.java
@@ -1,4 +1,15 @@
 package service;
 
+/*
+* Created by IntelliJ IDEA.
+  User: Anderson Soto
+  Date: 14/5/2025
+  Time: 23:08
+  * */
+import models.Producto;
+import java. util.List;
+
 public interface ProductoService {
+
+    List<Producto> listar();
 }
